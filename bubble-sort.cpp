204be17da8c44de0a680bdbec56fe123ca3d4321
Faizan208 <vooooproop@gmail.com>
2025-09-20 06:59:13 +0500

    Add files via upload

---
+++ bubble-sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	
+	cout << "Enter the size of the array : ";
+	cin >> n; 
+	
+	int arr[100];
+	
+	cout << "Enter the " << n << " elements..." << endl;
+ 	for(int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	
+	for(int i = 0; i < n - 1; i++)
+	{
+		for(int j = 0; j < n - i - 1; j++)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				swap(arr[j], arr[j+1]);
+			}
+		}
+	}
+	
+	cout << "Sorted Array { ";
+	for(int k = 0; k < n; k++)
+	{
+		cout << arr[k] << " "; 
+	}
+	cout << "}" << endl;
+}
